92e0ab1f097ad90de4afa629f873eb841d41746e
Marko Golovko <markgolovko@gmail.com>
2022-12-06 20:17:35 +0100

    add day 6

---
+++ day6.py
@@ -0,0 +1,20 @@
+# --- Part 1,2 ---
+
+f = open("input6.txt", "r")
+for line in f:
+    marker = []
+    index = 1
+    for ch in line:
+        while ch in marker:
+            marker.pop(0)
+        marker.append(ch)
+        # change if for switching between Part 1 and Part 2
+        # if len(marker) == 4:
+        if len(marker) == 14:
+            print(marker)
+            print(index)
+            break
+        index+=1
+
+
+f.close()
